42f6d70cd9de5152a247bc2b4324054c87cd3891
Romija <124134613+Romija@users.noreply.github.com>
2023-09-19 09:54:08 +0300

    Add files via upload

---
+++ formula_Suhanova_karklina.py
@@ -0,0 +1,10 @@
+rl = int(input('Ievadiet riņķa līnijas rādiusu: ')) #rl ievadītais riņķa līnijas rādiuss
+print(rl)
+print(float(rl))
+pi = float(3.142)
+print(float(2 * pi * rl))
+print(float(pi * rl *rl))
+k1 = int(input('Ievadiet taisnleņķa katešu garumu: ')) #k1 ir ievadītais katešu garums
+print(k1)
+k2 = int(input('Ievadiet otrās katetes garumu: ')) #k2 ir ievadītais katešu garums
+print('Hipotenūzas garums: ')
